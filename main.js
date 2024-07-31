08578fc2e2c4557c0bcfa140360a3c9b6ebf4fba
NullDev (Shadow) <chris@nulldev.org>
2024-07-31 14:21:15 +0200

    added settings page

--- main.js
+++ main.js
@@ -12,23 +12,40 @@ import "./style.scss";
  * @return {Promise<void>}
  */
 const apiCall = async function(pageData, question){
-    const res = await fetch("https://api.nulldev.org/tldr-ask", {
+    const apiKey = await new Promise((resolve) => {
+        chrome.storage.sync.get(["apiKey"], (data) => {
+            resolve(data.apiKey);
+        });
+    });
+
+    if (!apiKey){
+        console.error("API key not set.");
+        return;
+    }
+
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`;
+    const data = {
+        contents: [{
+            parts: [{
+                text: question,
+            }],
+        }],
+    };
+
+    const response = await fetch(url, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({ pageData, question }),
-    }).then(r => r.json());
+        body: JSON.stringify(data),
+    }).then((res) => res.json());
 
-    if (res.error){
-        console.error(res.error);
-        return;
-    }
+    alert(response);
 
     const output = document.getElementById("output");
     if (!output) return;
 
-    output.innerHTML = res.answer;
+    output.innerHTML = "";
 };
 
 /**
@@ -62,22 +79,57 @@ const ask = function(){
 };
 
 /**
+ * Toggle the settings page
+ *
+ * @return {void}
+ */
+const toggleSettingsPage = function(){
+    const settingsPage = document.getElementById("settings-page");
+    if (!settingsPage) return;
+
+    settingsPage.classList.toggle("hidden");
+};
+
+/**
  * Initialize the application
  *
  * @return {void}
  */
 const init = function(){
+    const settingsBtn = /** @type {HTMLLinkElement} */(document.getElementById("settings"));
+    if (!settingsBtn) return;
+    settingsBtn.addEventListener("click", () => toggleSettingsPage());
+
     const askBtn = /** @type {HTMLButtonElement} */(document.getElementById("ask"));
     if (!askBtn) return;
-
     askBtn.addEventListener("click", () => ask());
 
     const input = /** @type {HTMLInputElement} */ (document.getElementById("input"));
     if (!input) return;
     input.addEventListener("keydown", (event) => {
-        if (event.key === "Enter"){
-            ask();
-        }
+        if (event.key === "Enter") ask();
+    });
+
+    const saveBtn = /** @type {HTMLButtonElement} */(document.getElementById("save"));
+    const apiKey = /** @type {HTMLInputElement} */(document.getElementById("api-key"));
+    if (!saveBtn) return;
+    saveBtn.addEventListener("click", () => {
+        if (!apiKey) return;
+
+        const keyData = apiKey.value;
+        if (!keyData) return;
+
+        chrome.storage.sync.set({ apiKey: keyData }, () => {
+            const pStatus = document.getElementById("status");
+            if (!pStatus) return;
+
+            pStatus.textContent = "Key saved.";
+        });
+    });
+
+    chrome.storage.sync.get(["apiKey"], (data) => {
+        if (!apiKey) return;
+        apiKey.value = data.apiKey || "";
     });
 };
 
