98e964aa6e5db1837ae57b9f4e49f57f5a2c4687
NullDev (Shadow) <chris@nulldev.org>
2024-07-31 13:33:28 +0200

    content script

---
+++ content.js
@@ -0,0 +1,36 @@
+import "./content.scss";
+
+/* ========================= */
+/* = Copyright (c) NullDev = */
+/* ========================= */
+
+/**
+ * Get the content of the page
+ *
+ * @return {string|null}
+ */
+const getPageContent = function(){
+    function getTextFromNode(node){
+        if (node.nodeType === Node.TEXT_NODE){
+            return node.textContent.trim();
+        }
+        if (node.nodeType === Node.ELEMENT_NODE && node.tagName !== "SCRIPT" && node.tagName !== "STYLE"){
+            let text = "";
+            for (const child of node.childNodes){
+                text += " " + getTextFromNode(child);
+            }
+            return text.trim();
+        }
+        return "";
+    }
+
+    return getTextFromNode(document.body);
+};
+
+chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
+    if (message.type === "getPageContent"){
+        sendResponse({
+            content: getPageContent(),
+        });
+    }
+});
