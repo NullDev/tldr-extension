096cee884220986d393e84a2271f5497959d8ebf
NullDev (Shadow) <chris@nulldev.org>
2024-08-08 19:37:56 +0200

    added scanner loading animation

--- src/content.js
+++ src/content.js
@@ -4,6 +4,57 @@ import "./style/content.scss";
 /* = Copyright (c) NullDev = */
 /* ========================= */
 
+const scanner = `
+<div id="__f1fc290aff698d18__SCAN_WRAPPER__">
+    <div class="scanner">
+        <div class="scanner__display">
+            <table>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="dot"></td>
+                    <td class="dot"></td>
+                    <td class="dot"></td>
+                    <td class="dot"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="dot"></td>
+                    <td class="dot"></td>
+                    <td class="dot"></td>
+                    <td class="dot"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="dot"></td>
+                    <td class="dot"></td>
+                    <td class="dot"></td>
+                    <td class="dot"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="dot"></td>
+                    <td class="dot"></td>
+                    <td class="dot"></td>
+                    <td class="dot"></td>
+                </tr>
+            </table>
+            <div id="scanline" class="run"></div>
+        </div>
+    </div>
+</div>
+`;
+
+const startLoader = function(){
+    document.body.insertAdjacentHTML("beforeend", scanner);
+};
+
 /**
  * Get the content of the page
  *
@@ -29,6 +80,8 @@ const getPageContent = function(){
 
 chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
     if (message.type === "getPageContent"){
+        startLoader();
+
         sendResponse({
             content: getPageContent(),
         });
